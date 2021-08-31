f79dbd4796efc45b650bcd481c6a843f622baaa9
duan.tingting@yahoo.com <tingtingduan@tingtings-Air.attlocal.net>
2021-08-30 21:02:05 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import {Button} from 'react-bootstrap';
+import React from "react"
+import { Home } from "./components/home/Home"
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<>
+<h2>Home Breeze</h2>
+        <small>Find your next home, here.</small>
+        <div>
+  <Button variant="primary">Primary</Button>{' '}
+  <Button variant="secondary">Secondary</Button>{' '}
+  <Button variant="success">Success</Button>{' '}
+  <Button variant="warning">Warning</Button>{' '}
+  <Button variant="danger">Danger</Button> <Button variant="info">Info</Button>{' '}
+  <Button variant="light">Light</Button> <Button variant="dark">Dark</Button>{' '}
+  <Button variant="link">Link</Button>
+  </div>
+  <Home />
+  <Home />
+</>
   );
 }
 
---
+++ src/components/home/Home.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Home.css"
+
+export const Home = () => (
+    <section className="home">
+        <h3 className="home__name">Beautiful 3b3b house</h3>
+        <div className="home__price">Price: $300,000</div>
+    </section>
+)
