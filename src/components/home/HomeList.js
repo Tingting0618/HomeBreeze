b46d62e835c8eef21b97d1fda8754936ac0f71f8
duan.tingting@yahoo.com <tingtingduan@tingtings-Air.attlocal.net>
2021-09-13 20:19:28 -0500

    search function done

--- src/components/home/HomeList.js
+++ src/components/home/HomeList.js
@@ -23,63 +23,113 @@ export const HomeList = () => {
   // 2. Set up the Fuse instance
   const fuse = new Fuse(homes, {
     includeScore: true,
-    keys: ['city', 'address', 'zipcode', 'aName', 'aEmail']
+    keys: ['city', 'address1', 'zipcode', 'aName', 'aEmail']
   })
 
   // 3. Now search!{keywords}
-  // if (searchTerm === "") {
-  //   const filtered_homes = homes
-  // } else {
-  //   const filtered_homes = fuse.search(`${searchTerm}`)
-  // }
-  const filtered_homes = fuse.search(`${searchTerm}`)
-  return (
-    <>
-      <div className="page" >
-        <h3>
-          Our Exclusive Listings <Badge bg="success">New</Badge>
-        </h3>
-        <div style={{ display: "flex" }}>
-          <Dropdown style={{ marginLeft: "auto" }}>
-            <Dropdown.Toggle variant="dark" id="dropdown-basic">
-              Sort By
-            </Dropdown.Toggle>
+  if (searchTerm === null | searchTerm === "") {
+    return (
+      <>
+        <div className="page" >
+          <h3>
+            Our Exclusive Listings <Badge bg="success">New</Badge>
+          </h3>
+          <div style={{ display: "flex" }}>
+            <Dropdown style={{ marginLeft: "auto" }}>
+              <Dropdown.Toggle variant="dark" id="dropdown-basic">
+                Sort By
+              </Dropdown.Toggle>
 
-            <Dropdown.Menu >
-              <Dropdown.Item href="#/action-1">Price</Dropdown.Item>
-              <Dropdown.Item href="#/action-2">Listing Date</Dropdown.Item>
-              <Dropdown.Item href="#/action-3">Popularity</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
+              <Dropdown.Menu >
+                <Dropdown.Item href="#/action-1">Price</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Listing Date</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Popularity</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
 
+          </div>
+          <br />
+          <section className="homes">
+            {
+              homes.map(home => {
+                return (
+                  <>
+                    <Card key={home.id} style={{ width: '18rem', margin: '0.3rem' }}>
+                      <Card.Img variant="top" src={home.imageUrl} />
+                      <Card.Body>
+                        <Card.Title href="#">{home.address1}</Card.Title>
+                        <Card.Subtitle className="mb-2 text-muted">${home.price}, {home.bed} beds, {home.bath} baths</Card.Subtitle>
+                        <Card.Subtitle className="mb-2 text-muted">{home.sqft} sqft, {home.land} Acers</Card.Subtitle>
+
+                        <Card.Text>
+                          {home.desc.substring(0, 90)}...
+                        </Card.Text>
+                        <Link to={`/homes/detail/${home.id}`}> <Button variant="secondary" >See Details</Button></Link>
+                      </Card.Body>
+                    </Card>
+
+                  </>
+                )
+              })
+            }
+          </section>
         </div>
-        <br />
-        <section className="homes">
-          {
-            filtered_homes.map(home => {
-              return (
-                <>
-                  <Card key={home.item.id} style={{ width: '18rem', margin: '0.3rem' }}>
-                    <Card.Img variant="top" src={home.item.imageUrl} />
-                    <Card.Body>
-                      <Card.Title href="#">{home.item.address1}</Card.Title>
-                      <Card.Subtitle className="mb-2 text-muted">${home.item.price}, {home.item.bed} beds, {home.item.bath} baths</Card.Subtitle>
-                      <Card.Subtitle className="mb-2 text-muted">{home.item.sqft} sqft, {home.item.land} Acers</Card.Subtitle>
+      </>
+    )
+  } else {
+    const filtered_homes = fuse.search(`${searchTerm}`)
+    return (
+      <>
+        <div className="page" >
+          <h3>
+            Our Exclusive Listings <Badge bg="success">New</Badge>
+          </h3>
+          <div style={{ display: "flex" }}>
+            <Dropdown style={{ marginLeft: "auto" }}>
+              <Dropdown.Toggle variant="dark" id="dropdown-basic">
+                Sort By
+              </Dropdown.Toggle>
 
-                      <Card.Text>
-                        {home.item.desc.substring(0, 90)}...
-                      </Card.Text>
-                      <Link to={`/homes/detail/${home.item.id}`}> <Button variant="secondary" >See Details</Button></Link>
-                    </Card.Body>
-                  </Card>
+              <Dropdown.Menu >
+                <Dropdown.Item href="#/action-1">Price</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">Listing Date</Dropdown.Item>
+                <Dropdown.Item href="#/action-3">Popularity</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
 
-                </>
-              )
-            })
-          }
-        </section>
-      </div>
-    </>
-  )
+          </div>
+          <br />
+          <section className="homes">
+            {
+              filtered_homes.map(home => {
+                if (parseFloat(home.score) >= 0.0) {
+                  return (
+                    <>
+                      <Card key={home.item.id} style={{ width: '18rem', margin: '0.3rem' }}>
+                        <Card.Img variant="top" src={home.item.imageUrl} />
+                        <Card.Body>
+                          <Card.Title href="#">{home.item.address1}</Card.Title>
+                          <Card.Subtitle className="mb-2 text-muted">${home.item.price}, {home.item.bed} beds, {home.item.bath} baths</Card.Subtitle>
+                          <Card.Subtitle className="mb-2 text-muted">{home.item.sqft} sqft, {home.item.land} Acers</Card.Subtitle>
+
+                          <Card.Text>
+                            {home.item.desc.substring(0, 90)}...
+                          </Card.Text>
+                          <Link to={`/homes/detail/${home.item.id}`}> <Button variant="secondary" >See Details</Button></Link>
+                        </Card.Body>
+                      </Card>
+
+                    </>
+                  )
+                }
+
+              })
+            }
+          </section>
+        </div>
+      </>
+    )
+  }
+
 }
 
